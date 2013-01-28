776a8022b93e61907229a9fe830a3788571f1926
mydou <252202445@qq.com>
2013-01-28 15:38:25 +0800

    可以捕获参数并生成MD5码了

--- routes/read.js
+++ routes/read.js
@@ -1,35 +1,66 @@
 var DB = require('db');
+var crypto = require('crypto');
+
 var filterParamRe = /\w:\w+/
 var isNumber = /^[1-9][0-9]*$/;
 var format = /^(?:jpg|jpeg|gif|png)$/;
 var quality = /^(?:[1-9][0-9]?|100)$/;
 
+function int(s) {
+    return parseInt(s, 10);
+}
+
 var validator = {
     w: {
         re: isNumber,
-        fn: function () {
-
-        }
+        map: int
+    },
+    h: {
+        re: isNumber,
+        map: int
+    },
+    f: {
+        re: format
+    },
+    q: {
+        re: quality,
+        map: int
     }
 }
 
 exports.read = function (req, res) {
 
     var p = {};
+    var sortParam = Object.create(null);
     if (req.params[1]) {
         req.params[1].split('/').forEach(function (item) {
             var _p = item.split(':');
+            var o = validator[_p[0]];
             if (filterParamRe.test(item)) {
-                if (validator[_p[0]]) {
-                    if (validator[_p[0]].re.test(_p[1])) p[_p[0]] = _p[1];
+                if (o && o.re.test(_p[1])) {
+                    p[_p[0]] = o.map ? o.map(_p[1]) : _p[1];
                 }
             }
         })
-        console.log(p);
+        //排序，以确保不同参数位置，但相同参数值所得到的MD5值一致
+        Object.keys(p).sort().forEach(function (k) {
+            sortParam[k] = p[k];
+        })
     }
 
-    var id = DB.mongodb.ObjectID(req.params[0]);
+    //将参数转换为MD5
 
+    var md5 = crypto.createHash('md5');
+    md5.update(JSON.stringify(sortParam));
+    md5 = md5.digest('hex');
+    console.log(md5)
+
+
+
+
+
+
+    var id = DB.mongodb.ObjectID(req.params[0]);
     var gs = new DB.mongodb.GridStore(DB.dbServer, id, "r");
 
     gs.open(function (err, gs) {
